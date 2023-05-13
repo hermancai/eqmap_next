faedc5715aa4087ff7dcbeb17180e5b5caddf072
hermancai <30858461+hermancai@users.noreply.github.com>
2023-05-12 19:55:46 -0700

    Add markerSize to dependency array

--- components/home/EventCircle.tsx
+++ components/home/EventCircle.tsx
@@ -37,7 +37,7 @@ const EventCircle = ({
       },
       pixelOffset: new google.maps.Size(0, data.properties.mag * -markerSize),
     });
-  }, [data]);
+  }, [data, markerSize]);
 
   return (
     <Marker
