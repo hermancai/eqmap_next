af226c64e6db9862945b891518b4f2f050bd561a
hermancai <30858461+hermancai@users.noreply.github.com>
2023-05-12 19:52:39 -0700

    Add icons and remove default files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,44 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Earthquake Map</title>
+        <meta
+          name="description"
+          content="Search for past earthquake events based on location, date, and magnitude. Data is visualized on a Google map, table, and graphs."
+        />
+        <meta name="author" content="Herman Cai https://hermancai.dev" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#f1f5f9" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="msapplication-TileColor" content="#f1f5f9" />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0f172a" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
